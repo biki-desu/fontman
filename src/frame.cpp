de6f30d30a5541dc95f457013b77f737d4bf878d
biki-desu <biki-desu@users.noreply.github.com>
2014-09-04 22:20:10 +0100

    Create a gui

--- src/frame.cpp
+++ src/frame.cpp
@@ -12,7 +12,8 @@
 // headers
 // ---------------------------------------------------------------------
 
-#include "wx.hpp"
+#include "wx/artprov.h"
+
 #include "frame.hpp"
 
 // ---------------------------------------------------------------------
@@ -30,10 +31,30 @@
 
 // the event tables connect the wxWidgets events with the functions (event handlers) which process them. It can be also done at run-time, but for the simple menu events like this the static method is much simpler.
 wxBEGIN_EVENT_TABLE(fmFrame, wxFrame)
-    EVT_MENU(fmMenu_File_Quit,  fmFrame::OnQuit)
-    EVT_MENU(fmMenu_Help_About, fmFrame::OnAbout)
-
     EVT_CLOSE(fmFrame::OnClose)
+
+    EVT_MENU(fmMenuFile_Quit,  fmFrame::OnMenuQuit)
+    EVT_MENU(fmMenuHelp_About, fmFrame::OnMenuAbout)
+
+    EVT_LIST_END_LABEL_EDIT(fmCL_ID, fmFrame::OnCatEndLabelEdit)
+    EVT_LIST_ITEM_SELECTED(fmCL_ID, fmFrame::OnCatItemSelect)
+    EVT_LIST_ITEM_DESELECTED(fmCL_ID, fmFrame::OnCatItemDeselect)
+    EVT_LIST_ITEM_ACTIVATED(fmCL_ID, fmFrame::OnCatItemActivate)
+    EVT_LIST_ITEM_FOCUSED(fmCL_ID, fmFrame::OnCatItemFocus)
+
+    EVT_BUTTON(fmCLBtnIns_ID, fmFrame::OnCatCtrlIns)
+    EVT_BUTTON(fmCLBtnDel_ID, fmFrame::OnCatCtrlDel)
+    EVT_TEXT(fmCLCtrlSrh_ID, fmFrame::OnCatSearchAsYouType)
+
+    EVT_LIST_ITEM_SELECTED(fmFL_ID, fmFrame::OnFontItemSelect)
+
+    EVT_BUTTON(fmPVBtnInfo_ID, fmFrame::OnPVShowInfo)
+    EVT_BUTTON(fmPVBtnChar_ID, fmFrame::OnPVShowChar)
+    EVT_COLOURPICKER_CHANGED(fmPVCtrlClr_ID, fmFrame::OnPVClrChange)
+    EVT_FONTPICKER_CHANGED(fmPVCtrlFnt_ID, fmFrame::OnPVFntChange)
+    EVT_COMBOBOX(fmPVCtrlStl_ID, fmFrame::OnPVStlChange)
+    EVT_SPINCTRL(fmPVCtrlSze_ID, fmFrame::OnPVSzeChange)
+
 wxEND_EVENT_TABLE()
 
 // =====================================================================
@@ -42,48 +63,166 @@ wxEND_EVENT_TABLE()
 
 fmFrame::fmFrame(wxWindow *parent, wxWindowID id, const wxString &title, const wxPoint &pos, const wxSize &size, long style) : wxFrame(parent, id, title, pos, size, style)
 {
-    // set the frame icon
-    //SetIcon(wxICON(sample));
-
-#if wxUSE_MENUS
-    // create a menu bar
-    wxMenu *fileMenu = new wxMenu;
-    fileMenu->Append(fmMenu_File_Quit, "E&xit\tAlt-F4", "Quit this program");
-
-    // the "About" item should be in the help menu
-    wxMenu *helpMenu = new wxMenu;
-    helpMenu->Append(fmMenu_Help_About, "&About\tF1", "Show about dialog");
-
-    // now append the freshly created menu to the menu bar...
-    wxMenuBar *menuBar = new wxMenuBar();
-    menuBar->Append(fileMenu, "&File");
-    menuBar->Append(helpMenu, "&Help");
-
-    // ... and attach this menu bar to the frame
-    SetMenuBar(menuBar);
-#endif // wxUSE_MENUS
-
-#if wxUSE_STATUSBAR
-    // create a status bar just for fun (by default with 1 pane only)
-    CreateStatusBar(2);
-    #ifdef FM_DEBUG
-        SetStatusText(wxString::Format("Welcome to fontman %s (debug version)!", FM_VERSION));
-    #else
-        SetStatusText(wxString::Format("Welcome to fontman %s!", FM_VERSION));
-    #endif
-#endif // wxUSE_STATUSBAR
-
+    // get the config object
     wxConfigBase *pConfig = wxConfigBase::Get();
 
-    // restore frame position and size
-    pConfig->SetPath(wxT("/ui/frame/"));
-    int x = pConfig->Read(wxT("x"), 50),
-        y = pConfig->Read(wxT("y"), 50),
-        w = pConfig->Read(wxT("w"), 350),
-        h = pConfig->Read(wxT("h"), 200);
-    Move(x, y);
-    SetClientSize(w, h);
-    pConfig->SetPath(wxT("/"));
+    // set the window icon
+    SetIcon(wxArtProvider::GetIcon(wxART_HELP_SETTINGS, wxART_FRAME_ICON, wxDefaultSize));
+
+    // set the window title
+#ifdef FM_DEBUG
+    SetTitle(wxString::Format("%s %s [DEBUG VERSION]", FM_NAME, FM_VERSION));
+#else
+    SetTitle(wxString::Format("%s %s", FM_NAME, FM_VERSION));
+#endif
+
+    {// restore frame position and size
+        pConfig->SetPath(wxT("/ui/frame/"));
+
+        int x = pConfig->Read(wxT("x"), 50),
+            y = pConfig->Read(wxT("y"), 50),
+            w = pConfig->Read(wxT("w"), 350),
+            h = pConfig->Read(wxT("h"), 200);
+        Move(x, y);
+        SetClientSize(w, h);
+
+        pConfig->SetPath(wxT("/"));
+    }
+
+    {// create a menu bar
+        fmMenuFile = new wxMenu;
+        fmMenuFile->Append(fmMenuFile_Quit, "E&xit\tAlt-F4", "Quit this program");
+
+        fmMenuHelp = new wxMenu;
+        fmMenuHelp->Append(fmMenuHelp_About, "&About\tF1", "Show about dialog");
+
+        fmMenuBar = new wxMenuBar();
+        fmMenuBar->Append(fmMenuFile, "&File");
+        fmMenuBar->Append(fmMenuHelp, "&Help");
+
+    }
+    SetMenuBar(fmMenuBar);
+
+    {// create a status bar just for fun (by default with 1 pane only)
+        fmStatusBar = new wxStatusBar(this, wxID_ANY, wxSTB_DEFAULT_STYLE);
+
+#ifdef FM_DEBUG
+        fmStatusBar->PushStatusText(wxString::Format("Welcome to %s %s (debug version)!", FM_NAME, FM_VERSION), 0);
+#else
+        fmStatusBar->PushStatusText(wxString::Format("Welcome to %s %s!", FM_NAME, FM_VERSION), 0);
+#endif
+    }
+    SetStatusBar(fmStatusBar);
+
+
+    fmVSplit = new wxSplitterWindow(this, fmVS_ID, wxDefaultPosition, wxDefaultSize, wxSP_3D);
+
+    {
+        fmCLPanel = new wxPanel(fmVSplit, fmCLPanel_ID, wxDefaultPosition, wxDefaultSize, wxTAB_TRAVERSAL);
+        fmCLVSizer = new wxBoxSizer(wxVERTICAL);
+        fmCLPanel->SetSizer(fmCLVSizer);
+
+        {
+            fmCL = new wxListCtrl(fmCLPanel, fmCL_ID, wxDefaultPosition, wxDefaultSize, wxLC_REPORT | wxLC_EDIT_LABELS, wxDefaultValidator); //wxLC_NO_HEADER
+            fmCL->AppendColumn(wxT("Category name"), wxLIST_FORMAT_LEFT, -1);
+            fmCL->InsertItem(0, wxT("All"));
+            fmCL->InsertItem(1, wxT("System"));
+            fmCL->InsertItem(2, wxT("User"));
+            fmCL->InsertItem(3, wxT("Uncategorised"));
+            fmCL->InsertItem(4, wxT(""));
+            fmCL->InsertItem(5, wxT("USR CAT 1"));
+            fmCL->InsertItem(6, wxT("USR CAT 2"));
+            fmCL->InsertItem(7, wxT("USR CAT 3"));
+        }
+        fmCLVSizer->Add(fmCL, 1, wxEXPAND | wxALIGN_LEFT | wxALIGN_TOP, 0);
+
+        fmCLHSizer = new wxBoxSizer(wxHORIZONTAL);
+
+        {
+            fmCLBtnIns = new wxBitmapButton(fmCLPanel, fmCLBtnIns_ID, wxArtProvider::GetBitmap(wxART_PLUS, wxART_BUTTON, wxDefaultSize), wxDefaultPosition, wxDefaultSize, wxBU_AUTODRAW, wxDefaultValidator);
+            fmCLHSizer->Add(fmCLBtnIns, 0, wxEXPAND | wxALIGN_LEFT | wxALIGN_TOP, 0);
+
+            fmCLBtnDel = new wxBitmapButton(fmCLPanel, fmCLBtnDel_ID, wxArtProvider::GetBitmap(wxART_MINUS, wxART_BUTTON, wxDefaultSize), wxDefaultPosition, wxDefaultSize, wxBU_AUTODRAW, wxDefaultValidator);
+            fmCLHSizer->Add(fmCLBtnDel, 0, wxEXPAND | wxALIGN_LEFT | wxALIGN_TOP, 0);
+
+            fmCLHSizer->AddStretchSpacer(1);
+
+            fmCLCtrlSrh = new wxSearchCtrl(fmCLPanel, fmCLCtrlSrh_ID, wxEmptyString, wxDefaultPosition, wxDefaultSize, 0, wxDefaultValidator);
+            fmCLHSizer->Add(fmCLCtrlSrh, 1, wxEXPAND | wxALIGN_RIGHT | wxALIGN_TOP, 0);
+        }
+
+        fmCLVSizer->Add(fmCLHSizer, 0, wxEXPAND | wxALIGN_LEFT | wxALIGN_BOTTOM, 0);
+    }
+
+    fmHSplit = new wxSplitterWindow(fmVSplit, fmHS_ID, wxDefaultPosition, wxDefaultSize, wxSP_3D);
+
+    {
+        fmVSplit->SplitVertically(fmCLPanel, fmHSplit);
+        fmVSplit->SetMinimumPaneSize(20);
+        fmVSplit->SetSashGravity(0.25);
+        fmVSplit->SetSashPosition(20, true);
+    }
+
+    {
+        fmFL = new wxListCtrl(fmHSplit, fmFL_ID, wxDefaultPosition, wxDefaultSize, wxLC_REPORT | wxLC_SINGLE_SEL, wxDefaultValidator);
+        fmFL->AppendColumn(wxT("Font name"), wxLIST_FORMAT_LEFT, -1);
+        fmFL->InsertItem(0, wxT("Font 1"));
+        fmFL->InsertItem(1, wxT("Font 2"));
+        fmFL->InsertItem(2, wxT("Font 3"));
+        fmFL->InsertItem(3, wxT("Font 4"));
+    }
+
+    {
+        pConfig->SetPath(wxT("/ui/preview/"));
+
+        fmPVPanel = new wxPanel(fmHSplit, fmPVPanel_ID, wxDefaultPosition, wxDefaultSize, wxTAB_TRAVERSAL);
+        fmPVVSizer = new wxBoxSizer(wxVERTICAL);
+        fmPVPanel->SetSizer(fmPVVSizer);
+
+        {
+            wxString fmPVDefaultText = pConfig->Read(wxT("default-text"), wxT("abcdefghijklmnopqrstuvxyz\nABCDEFGHIJKLMNOPQRSTUVXYZ\n`1234567890-=[];'#\\,./\n¬!\"£$%^&*()_+{}:@~|<>?"));
+            fmPV = new wxTextCtrl(fmPVPanel, fmPV_ID, fmPVDefaultText, wxDefaultPosition, wxDefaultSize, wxTE_MULTILINE, wxDefaultValidator);
+            fmPVVSizer->Add(fmPV, 1, wxEXPAND | wxALIGN_LEFT | wxALIGN_TOP, 0);
+        }
+
+        fmPVHSizer = new wxBoxSizer(wxHORIZONTAL);
+
+        {
+            fmPVBtnInfo = new wxBitmapButton(fmPVPanel, fmPVBtnInfo_ID, wxArtProvider::GetBitmap(wxART_INFORMATION, wxART_BUTTON, wxDefaultSize), wxDefaultPosition, wxDefaultSize, wxBU_AUTODRAW, wxDefaultValidator);
+            fmPVHSizer->Add(fmPVBtnInfo, 0, wxEXPAND | wxALIGN_LEFT | wxALIGN_TOP, 0);
+
+            fmPVBtnChar = new wxBitmapButton(fmPVPanel, fmPVBtnChar_ID, wxArtProvider::GetBitmap(wxART_HELP_SETTINGS, wxART_BUTTON, wxDefaultSize), wxDefaultPosition, wxDefaultSize, wxBU_AUTODRAW, wxDefaultValidator);
+            fmPVHSizer->Add(fmPVBtnChar, 0, wxEXPAND | wxALIGN_LEFT | wxALIGN_TOP, 0);
+
+            fmPVCtrlClr = new wxColourPickerCtrl(fmPVPanel, fmPVCtrlClr_ID, *wxBLACK, wxDefaultPosition, wxDefaultSize, wxCLRP_DEFAULT_STYLE, wxDefaultValidator);
+            fmPVHSizer->Add(fmPVCtrlClr, 0, wxEXPAND | wxALIGN_LEFT | wxALIGN_TOP, 0);
+
+            fmPVCtrlFnt = new wxFontPickerCtrl(fmPVPanel, fmPVCtrlFnt_ID, wxNullFont, wxDefaultPosition, wxDefaultSize, wxFNTP_DEFAULT_STYLE, wxDefaultValidator);
+            fmPVHSizer->Add(fmPVCtrlFnt, 1, wxEXPAND | wxALIGN_LEFT | wxALIGN_TOP, 0);
+
+            fmPVHSizer->AddStretchSpacer(1);
+
+            fmPVCtrlStl = new wxComboCtrl(fmPVPanel, fmPVCtrlStl_ID, wxEmptyString, wxDefaultPosition, wxDefaultSize, wxCB_READONLY | wxCB_SORT, wxDefaultValidator);
+            fmPVHSizer->Add(fmPVCtrlStl, 0, wxEXPAND | wxALIGN_RIGHT | wxALIGN_TOP, 0);
+
+            int fmPVCtrlSzeMin = pConfig->Read(wxT("min-size"), 6),
+                fmPVCtrlSzeMax = pConfig->Read(wxT("max-size"), 72),
+                fmPVCtrlSzeDft = pConfig->Read(wxT("default-size"), 12);
+            fmPVCtrlSze = new wxSpinCtrl(fmPVPanel, fmPVCtrlSze_ID, wxEmptyString, wxDefaultPosition, wxDefaultSize, wxSP_ARROW_KEYS, fmPVCtrlSzeMin, fmPVCtrlSzeMax, fmPVCtrlSzeDft);
+            fmPVHSizer->Add(fmPVCtrlSze, 0, wxEXPAND | wxALIGN_RIGHT | wxALIGN_TOP, 0);
+        }
+
+        fmPVVSizer->Add(fmPVHSizer, 0, wxEXPAND | wxALIGN_LEFT | wxALIGN_BOTTOM, 0);
+
+        pConfig->SetPath(wxT("/"));
+    }
+
+    {
+        fmHSplit->SplitHorizontally(fmFL, fmPVPanel);
+        fmHSplit->SetMinimumPaneSize(20);
+        fmHSplit->SetSashGravity(1);
+        fmHSplit->SetSashPosition(20, true);
+    }
 }
 
 fmFrame::~fmFrame()
@@ -92,19 +231,22 @@ fmFrame::~fmFrame()
     if ( pConfig == NULL )
         return;
 
-    // save the frame position
-    pConfig->SetPath(wxT("/ui/frame/"));
-    int x, y, w, h;
-    GetClientSize(&w, &h);
-    GetPosition(&x, &y);
-    pConfig->Write(wxT("x"), (long) x);
-    pConfig->Write(wxT("y"), (long) y);
-    pConfig->Write(wxT("w"), (long) w);
-    pConfig->Write(wxT("h"), (long) h);
-    pConfig->SetPath(wxT("/"));
+    {// save the frame position
+        pConfig->SetPath(wxT("/ui/frame/"));
+
+        int x, y, w, h;
+        GetClientSize(&w, &h);
+        GetPosition(&x, &y);
+        pConfig->Write(wxT("x"), (long) x);
+        pConfig->Write(wxT("y"), (long) y);
+        pConfig->Write(wxT("w"), (long) w);
+        pConfig->Write(wxT("h"), (long) h);
+
+        pConfig->SetPath(wxT("/"));
+    }
 }
 
-void fmFrame::OnAbout(wxCommandEvent& WXUNUSED(event))
+void fmFrame::OnMenuAbout(wxCommandEvent& WXUNUSED(event))
 {
 #ifdef FM_DEBUG
     wxMessageBox(wxString::Format("Welcome to fontman %s (debug version)!\n\nWe're using %s.\n\nOur OS is %s.", FM_VERSION, wxVERSION_STRING, wxGetOsDescription()), "About fontman", wxOK | wxICON_INFORMATION, this);
@@ -114,7 +256,7 @@ void fmFrame::OnAbout(wxCommandEvent& WXUNUSED(event))
 
 }
 
-void fmFrame::OnQuit(wxCommandEvent& WXUNUSED(event))
+void fmFrame::OnMenuQuit(wxCommandEvent& WXUNUSED(event))
 {
     Close(true);
 }
--- src/frame.hpp
+++ src/frame.hpp
@@ -16,6 +16,14 @@
 
 #include "wx.hpp"
 
+#include <wx/clrpicker.h>
+#include <wx/combo.h>
+#include <wx/fontpicker.h>
+#include "wx/listctrl.h"
+#include <wx/spinctrl.h>
+#include "wx/splitter.h"
+#include <wx/srchctrl.h>
+
 // ---------------------------------------------------------------------
 // constants
 // ---------------------------------------------------------------------
@@ -24,8 +32,26 @@
 enum
 {
     // menu items
-    fmMenu_File_Quit = wxID_EXIT,
-    fmMenu_Help_About = wxID_ABOUT, // it is important for the id corresponding to the "About" command to have this standard value as otherwise it won't be handled properly under Mac (where it is special and put into the "Apple" menu)
+    fmMenuFile_Quit = wxID_EXIT,
+    fmMenuHelp_About = wxID_ABOUT, // it is important for the id corresponding to the "About" command to have this standard value as otherwise it won't be handled properly under Mac (where it is special and put into the "Apple" menu)
+
+    // controls
+    fmVS_ID,
+    fmCLPanel_ID,
+    fmCL_ID,
+    fmCLBtnIns_ID,
+    fmCLBtnDel_ID,
+    fmCLCtrlSrh_ID,
+    fmHS_ID,
+    fmFL_ID,
+    fmPVPanel_ID,
+    fmPV_ID,
+    fmPVBtnInfo_ID,
+    fmPVBtnChar_ID,
+    fmPVCtrlClr_ID,
+    fmPVCtrlFnt_ID,
+    fmPVCtrlStl_ID,
+    fmPVCtrlSze_ID,
 };
 
 // =====================================================================
@@ -39,11 +65,79 @@ public:
     fmFrame(wxWindow *parent, wxWindowID id, const wxString &title, const wxPoint &pos=wxDefaultPosition, const wxSize &size=wxDefaultSize, long style=wxDEFAULT_FRAME_STYLE);
     virtual ~fmFrame();
 
-    // event handlers (these functions should _not_ be virtual)
-    void OnQuit(wxCommandEvent& WXUNUSED(event));
-    void OnAbout(wxCommandEvent& WXUNUSED(event));
-    
+// event handlers (these functions should _not_ be virtual)
+    // exit
     void OnClose(wxCloseEvent& WXUNUSED(event));
+
+    // menu
+    void OnMenuQuit(wxCommandEvent& WXUNUSED(event));
+    void OnMenuAbout(wxCommandEvent& WXUNUSED(event));
+
+    // categories list
+    void OnCatEndLabelEdit(wxListEvent& event) { event.Skip(); }
+    void OnCatItemSelect(wxListEvent& event) { event.Skip(); }
+    void OnCatItemDeselect(wxListEvent& event) { event.Skip(); }
+    void OnCatItemActivate(wxListEvent& event) { event.Skip(); }
+    void OnCatItemFocus(wxListEvent& event) { event.Skip(); }
+
+    // categories controls
+    void OnCatCtrlIns(wxCommandEvent& event) { event.Skip(); }
+    void OnCatCtrlDel(wxCommandEvent& event) { event.Skip(); }
+    void OnCatSearchAsYouType(wxCommandEvent& event) { event.Skip(); }
+
+    // font list
+    void OnFontItemSelect(wxListEvent& event) { event.Skip(); }
+
+    // preview controls
+    void OnPVShowInfo(wxCommandEvent& event) { event.Skip(); }
+    void OnPVShowChar(wxCommandEvent& event) { event.Skip(); }
+    void OnPVClrChange(wxColourPickerEvent& event) { event.Skip(); }
+    void OnPVFntChange(wxFontPickerEvent& event) { event.Skip(); }
+    void OnPVStlChange(wxCommandEvent& event) { event.Skip(); }
+    void OnPVSzeChange(wxSpinEvent& event) { event.Skip(); }
+
+protected:
+    // menu bar
+    wxMenuBar *fmMenuBar;
+    wxMenu *fmMenuFile;
+    wxMenu *fmMenuHelp;
+
+    // status bar
+    wxStatusBar *fmStatusBar;
+
+    // categories sizers
+    wxSplitterWindow *fmVSplit;
+    wxPanel *fmCLPanel;
+    wxBoxSizer *fmCLVSizer;
+    wxBoxSizer *fmCLHSizer;
+
+    // categories list
+    wxListCtrl *fmCL;
+
+    // categories controls
+    wxBitmapButton *fmCLBtnIns;
+    wxBitmapButton *fmCLBtnDel;
+    wxSearchCtrl *fmCLCtrlSrh;
+
+    // preview sizers
+    wxSplitterWindow *fmHSplit;
+    wxPanel *fmPVPanel;
+    wxBoxSizer *fmPVVSizer;
+    wxBoxSizer *fmPVHSizer;
+
+    // font list
+    wxListCtrl *fmFL;
+
+    // preview text control
+    wxTextCtrl *fmPV;
+
+    // preview controls
+    wxBitmapButton *fmPVBtnInfo;
+    wxBitmapButton *fmPVBtnChar;
+    wxColourPickerCtrl *fmPVCtrlClr;
+    wxFontPickerCtrl *fmPVCtrlFnt;
+    wxComboCtrl *fmPVCtrlStl;
+    wxSpinCtrl *fmPVCtrlSze;
 
 private:
     // any class wishing to process wxWidgets events must use this macro
